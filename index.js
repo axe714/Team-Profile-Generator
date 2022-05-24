01b0ad7435142417f72d201ef74a525f310f3792
Axe <axearzadon@gmail.com>
2022-05-23 21:19:28 -0700

    Added pushing team members into array inside generator functions

--- index.js
+++ index.js
@@ -6,31 +6,35 @@ const fs = require("fs");
 const teamArray = [];
 
 function starterQuestion() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "companyName",
-      message: "What is your company's name?",
-    },
-  ]).then((responses) => {
-    teamGenerator();
-})
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "companyName",
+        message: "What is your company's name?",
+      },
+    ])
+    .then((responses) => {
+      teamGenerator();
+    });
+}
 
 function teamGenerator() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "employeeType",
-      message: "What type of employee would you like to add?",
-      choices: [
-        "Manager",
-        "Engineer",
-        "Intern",
-        "I don't want to add any more employees",
-      ],
-    },
-  ]).then((responses) => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message: "What type of employee would you like to add?",
+        choices: [
+          "Manager",
+          "Engineer",
+          "Intern",
+          "I don't want to add any more employees",
+        ],
+      },
+    ])
+    .then((responses) => {
       switch (responses.employeeType) {
         case "Manager":
           generateManager();
@@ -46,106 +50,115 @@ function teamGenerator() {
 }
 
 function generateManager() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "What is your team manager's name?",
-    },
-    {
-      type: "input",
-      name: "managerId",
-      message: "What is your team manager's ID?",
-    },
-    {
-      type: "input",
-      name: "managerEmail",
-      message: "What is your team manager's email?",
-    },
-    {
-      type: "input",
-      name: "managerOfficeNumber",
-      message: "What is your team manager's office number?",
-    },
-  ]).then((responses) => {
-    const manager = new Manager(
-      responses.managerName,
-      responses.managerId,
-      responses.managerEmail,
-      responses.officeNumber,
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is your team manager's name?",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is your team manager's ID?",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is your team manager's email?",
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is your team manager's office number?",
+      },
+    ])
+    .then((responses) => {
+      const manager = new Manager(
+        responses.managerName,
+        responses.managerId,
+        responses.managerEmail,
+        responses.officeNumber
       );
-      //WRITE ADDITIONAL LOGIC HERE TO PUSH MANAGER TO TEAM ARRAY
-    teamGenerator();
-})
-};
+      teamArray.push(manager);
+      console.log("Team Array ------- ", teamArray);
+      teamGenerator();
+    });
+}
 
 function generateEngineer() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "engineerName",
-      message: "What is your engineer's name?",
-    },
-    {
-      type: "input",
-      name: "engineerId",
-      message: "What is your engineer's ID?",
-    },
-    {
-      type: "input",
-      name: "engineerEmail",
-      message: "What is your engineer's email?",
-    },
-    {
-      type: "input",
-      name: "engineerGithub",
-      message: "What is your engineer's Github username?",
-    },
-  ]).then((responses) => {
-    const engineer = new Engineer(
-      responses.engineerName,
-      responses.engineerId,
-      responses.engineerEmail,
-      responses.githubUserName
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?",
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your engineer's Github username?",
+      },
+    ])
+    .then((responses) => {
+      const engineer = new Engineer(
+        responses.engineerName,
+        responses.engineerId,
+        responses.engineerEmail,
+        responses.githubUserName
       );
-      //WRITE ADDITIONAL LOGIC HERE TO PUSH ENGINEER TO TEAM ARRAY
-    teamGenerator();
-})
-};
+      teamArray.push(engineer);
+      console.log("Team Array ------- ", teamArray);
+      teamGenerator();
+    });
+}
 
 function generateIntern() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "internName",
-      message: "What is your intern's name?",
-    },
-    {
-      type: "input",
-      name: "internId",
-      message: "What is your intern's ID?",
-    },
-    {
-      type: "input",
-      name: "internEmail",
-      message: "What is your intern's email?",
-    },
-    {
-      type: "input",
-      name: "internSchool",
-      message: "What is your intern's school?",
-    },
-  ]).then((responses) => {
-    const intern = new Intern(
-      responses.internName,
-      responses.internId,
-      responses.internEmail,
-      responses.internSchool
-    );
-    //WRITE ADDITIONAL LOGIC HERE TO PUSH INTERN TO TEAM ARRAY
-    teamGenerator();
-})
-};
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "What is your intern's name?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is your intern's ID?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is your intern's email?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is your intern's school?",
+      },
+    ])
+    .then((responses) => {
+      const intern = new Intern(
+        responses.internName,
+        responses.internId,
+        responses.internEmail,
+        responses.internSchool
+      );
+      teamArray.push(intern);
+      console.log("Team Array ------- ", teamArray);
+      teamGenerator();
+    });
+}
 
 function init() {
   starterQuestion();
