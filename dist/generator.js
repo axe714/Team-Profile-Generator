9f4f3bc8f3427b7e8517987999dc783406a13d7a
Axe714 <axearzadon@gmail.com>
2022-05-24 09:46:46 -0700

    Added skeleton html into generator.js file

--- dist/generator.js
+++ dist/generator.js
@@ -7,18 +7,34 @@ const fs = require("fs");
 
 const buildTeam = (teamArray, companyName) => {
   const generateHTML = `<!DOCTYPE html>
-    <html lang="en">
+  <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${companyName[0]}</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>COMPANY NAME HERE</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+        crossorigin="anonymous"
+      />
     </head>
     <body>
-
+      <div class="card text-center">
+        <h3 class="card-header">COMPANY NAME HERE</h3>
+        <div class="card-body">
+          <h4 class="card-title">Meet our team</h4>
+        </div>
+        <div class="card-footer text-muted"></div>
+      </div>
+  
+      <div class="container-fluid">
+        <div class="row">
+        </div>
+      </div>
     </body>
-    </html>
+  </html>
     `;
 
     fs.writeFile("./dist/index.html", generateHTML, (err) => {
